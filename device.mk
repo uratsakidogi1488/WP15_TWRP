e27e7440a28818661e35208b22ebb8c394d3da57
uratsakidogi1488 <tottenkopf20@gmail.com>
2026-06-09 18:28:25 +1100

    Remove duplicate PRODUCT_COPY_FILES for init.recovery.vold_decrypt.rc (avoid double install)

--- device.mk
+++ device.mk
@@ -1,20 +1,3 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 LOCAL_PATH := device/oukitel/WP15
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
@@ -87,8 +70,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils.recovery
 
-PRODUCT_COPY_FILES += \
-    device/oukitel/WP15/recovery/root/init/init.recovery.vold_decrypt.rc:root/init.recovery.vold_decrypt.rc
+# PRODUCT_COPY_FILES for init.recovery.vold_decrypt is removed to avoid duplicate installs
+## PRODUCT_COPY_FILES += \
+##    device/oukitel/WP15/recovery/root/init/init.recovery.vold_decrypt.rc:root/init.recovery.vold_decrypt.rc
 
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
