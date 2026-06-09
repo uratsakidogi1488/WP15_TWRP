68a84da96f4ae522c97412c9698d83020eb084e6
uratsakidogi1488 <tottenkopf20@gmail.com>
2026-06-09 18:26:31 +1100

    Fix init.recovery.vold_decrypt.rc: treat as ETC prebuilt and set proper path/suffix

--- bootable/recovery/crypto/vold_decrypt/Android.mk
+++ bootable/recovery/crypto/vold_decrypt/Android.mk
@@ -1,19 +1,12 @@
-# Android.mk for vold_decrypt (minimal, patched to make init.recovery.vold_decrypt.rc a prebuilt ETC)
-# Path: bootable/recovery/crypto/vold_decrypt/Android.mk
+LOCAL_PATH := $(call my-dir)
 
-# (This is a minimal file added to ensure the init rc is treated as ETC when building
-#  in environments where this file is used. If the full Android.mk exists upstream,
-#  this file only contains the prebuilt block required by the patch.)
-
-include $(CLEAR_VARS)
-# If there is a shared library or other build above, include it here as needed
-# include $(BUILD_SHARED_LIBRARY)
-
-# Prebuilt init.recovery.vold_decrypt.rc
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.vold_decrypt.rc
-# Changed from RECOVERY_EXECUTABLES to ETC to avoid unexpected module class errors
+# Ensure init rc is treated as ETC (init script)
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := init.recovery.vold_decrypt.rc
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+LOCAL_MODULE_SUFFIX := .rc
+# Install into recovery root's init directory
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT)/init
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_PREBUILT)
